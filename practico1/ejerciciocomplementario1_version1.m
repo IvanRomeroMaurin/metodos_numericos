41f715d19d5e119ea2e0ac103921e29d869cd43f
Miguelonvho <garavagliamiguel23@gmail.com>
2026-08-20 02:11:23 -0300

    ejercicio complementario 1 version 1

---
+++ practico1/ejerciciocomplementario1_version1.m
@@ -0,0 +1,9 @@
+i = 0;
+resultado  = 0;
+
+while (resultado != 1/17)
+    i = i + 1;
+    resultado = ((-1)^i)/(2*i+1);
+endwhile
+
+printf("La cantidad de terminos que tiene la sucesion es de %d\n", i);
