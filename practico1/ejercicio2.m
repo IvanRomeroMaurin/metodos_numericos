9ab31deb30d7128ae1ed04124152c8d605e3bf78
Ivan Romero Maurin <ivanromeromaurin11@gmail.com>
2026-08-22 00:08:36 -0300

    feat: add debug print statement to ejercicio2.m and update documentation comments

--- practico1/ejercicio2.m
+++ practico1/ejercicio2.m
@@ -1,5 +1,4 @@
 % Modelo Matemático:
-% Sea X = {X_1, X_2, ..., X_N} con N = 200 y X_i > 0 para i = 1..N.
 % La suma de los elementos pares es: S = sum_{i=1}^{N} (X_i) para todo X_i tal que (X_i mod 2 == 0)
 
 % Ejercicio 2: Suma de números pares entre 200 números positivos
@@ -11,6 +10,7 @@ function resultado = sumar_pares(valores)
         if mod(valores(i), 2) == 0
             suma = suma + valores(i);
         end
+        valores(i)
     end
     resultado = suma;
 end
